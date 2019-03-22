66ade9f25d39aeea3013e117398d269f54c2dd4f
Airat Mufazalov <airat.mufazalov@yandex.ru>
2019-03-22 12:11:05 +0300

    Add missing copyrights

--- view/frontend/requirejs-config.js
+++ view/frontend/requirejs-config.js
@@ -1,3 +1,27 @@
+/**
+ * Mageinn_Seo extension
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Open Software License (OSL 3.0)
+ * that is bundled with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://opensource.org/licenses/osl-3.0.php
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@magentocommerce.com so we can send you a copy immediately.
+ *
+ * @category    Mageinn
+ * @package     Mageinn_Seo
+ * @copyright   Copyright (c) 2017 Mageinn. (http://mageinn.com/)
+ * @license     http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
+
+/**
+ * @category   Mageinn
+ * @package    Mageinn_Seo
+ * @author     Mageinn
+ */
 var config = {
    "map": {
       "*": {
